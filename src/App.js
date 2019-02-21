47266beeb088a6b9d89f00d27b9ccfa041683ee5
Darryl Banks <darryl.banks@zombie9.com>
2019-02-21 21:45:04 +0000

    update app.js component with bio

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
+          <h1>Darryl Banks</h1>
+          <p>I am a Junior Full-Stack JavaScript Developer</p>
+          <p>And I like the following tech: </p>
           <ul>
-            <li>item number one</li>
-            <li>item number two</li>
-            <li>item number three</li>
+            <li>Node.js</li>
+            <li>React.js</li>
+            <li>CSS</li>
           </ul>
       </div>
     );
